f083722a525a6891d8af8017d2c159c4d0a78715
LuxScintilla <catherina.ea@gmail.com>
2025-11-11 13:52:07 +0000

    add jonas react course exercise

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [currencyFrom, setCurrencyFrom] = useState("EUR");
+  const [currencyTo, setCurrencyTo] = useState("USD");
+  const [output, setOutput] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(
+    function () {
+      async function convert() {
+        setIsLoading(true);
+        const response = await fetch(
+          `https://api.frankfurter.dev/v1/latest?base=${currencyFrom}&symbols=${currencyTo}`
+        );
+        const data = await response.json();
+        const convertedAmount = (amount * data.rates[currencyTo]).toFixed(2);
+
+        isLoading
+          ? setOutput("Loading Conversion")
+          : setOutput(`${convertedAmount} ${currencyTo}`);
+
+        setIsLoading(false);
+      }
+      currencyFrom !== currencyTo && convert();
+      currencyFrom === currencyTo && setOutput(amount);
+    },
+    [amount, currencyFrom, currencyTo, isLoading]
+  );
+
+  function handleSetAmount(e) {
+    setAmount(e.target.value);
+  }
+
+  function handleSetCurrencyFrom(e) {
+    setCurrencyFrom(e.target.value);
+  }
+
+  function handleSetCurrencyTo(e) {
+    setCurrencyTo(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="container">
+      <div className="box">
+        <div className="input-box">
+          <input
+            type="number"
+            value={amount}
+            onChange={handleSetAmount}
+            disabled={isLoading ? true : false}
+          />
+          <select
+            value={currencyFrom}
+            onChange={handleSetCurrencyFrom}
+            disabled={isLoading ? true : false}
+          >
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="CAD">CAD</option>
+            <option value="INR">INR</option>
+          </select>
+          <select
+            value={currencyTo}
+            onChange={handleSetCurrencyTo}
+            disabled={isLoading ? true : false}
+          >
+            <option value="USD">USD</option>
+            <option value="EUR">EUR</option>
+            <option value="CAD">CAD</option>
+            <option value="INR">INR</option>
+          </select>
+        </div>
+        <div className="output-box">
+          <p>{output}</p>
+        </div>
+      </div>
+    </section>
   );
 }
 
